05638ab2ece2d814a6092ace7e7b12e369e6d22e
= <PaulHappyHutchinson@gmail.com>
2012-05-26 15:32:07 +0100

    Moved all includes into one file.

---
+++ classes/include.php
@@ -0,0 +1,16 @@
+<?php
+	// Include Classes
+	include $classesDir."defaults.php";		// Must be first
+	include $classesDir."log.php";			// Logger
+	include $classesDir."gps.php";			// GPS
+	include $classesDir."geomath.php";		// Math functions
+	include $classesDir."camera.php";		// Camera Capture
+	//include $classesDir."camera.camcorder.php";	// Video Capture
+	include $classesDir."tilt.php";			// Tilt/Gryo
+	include $classesDir."command.php";		// Contact with ground
+	include $classesDir."kml.php";			// Parse KML files
+	include $classesDir."flight.php";		// Fight engine
+	include $classesDir."filewriter.php";		// File writing engine
+	include $classesDir."sockets.php";		// Hardware, Socket
+	include $classesDir."functions.php";		// Other Functions
+?>
--- main.php
+++ main.php
@@ -7,34 +7,27 @@
 	$classesDir = $dir.DIRECTORY_SEPARATOR."classes".DIRECTORY_SEPARATOR;
 
 	// Include Classes
-	include $classesDir."defaults.php";		// Must be first
-	include $classesDir."log.php";			// Logger
-	include $classesDir."gps.php";			// GPS
-	include $classesDir."geomath.php";		// Math functions
-	include $classesDir."camera.php";		// Camera Capture
-	//include $classesDir."camera.camcorder.php";	// Video Capture
-	include $classesDir."tilt.php";			// Tilt/Gryo
-	include $classesDir."command.php";		// Contact with ground
-	include $classesDir."kml.php";			// Parse KML files
-	include $classesDir."flight.php";		// Fight engine
-	include $classesDir."filewriter.php";		// File writing engine
-	include $classesDir."hardware.php";		// Hardware, Socket
-	include $classesDir."functions.php";		// Other Functions
+	include $classesDir."include.php";		// Include all the files we require
 
+	// Create a Main log
 	$log  = new Log ("Main");
+
 	// Show license
 	showLicense();
 
+	// Just for fun
 	$log->log("PHP Version: ".PHP_VERSION, 0);
 	if ((string)PHP_INT_MAX !== "2147483647") {
 		$log->log("Hmm... Special INT Max (". (PHP_INT_MAX) .")", 1);
 	}
-	$gps1 = new GPS (25, 21, 9, microtime(true));
-	$gps2 = new GPS (25, 20, 3, microtime(true));
-	$tilt1 = new Tilt (100, 20);
-	$tilt2 = new Tilt (-30, 10);
-	$math = new GeoMath();
-	$camera = new Camera();
+
+	// Demo all the classes
+	$gps1	= new GPS (25, 21, 9, microtime(true));
+	$gps2	= new GPS (25, 20, 3, microtime(true));
+	$tilt1	= new Tilt (100, 20);
+	$tilt2	= new Tilt (-30, 10);
+	$math	= new GeoMath();
+	$camera	= new Camera();
 	$camera->takePhoto();
 
 	$math->distance($gps1->getDetails(), $gps2->getDetails());
@@ -50,9 +43,10 @@
 	$flight->addWaypoint(new Gps(-112.242073428656, 36.02626019082147, 2100));
 	$kml->generateKml($flight->getDetails(), "./flights/demo.kml");
 
-	// Demo Camera by waiting a second
+	// Demo Camera by waiting a second, so we can see a difference
 	sleep(1);
 	$camera->takePhoto();
 
-	var_dump(memory_get_peak_usage(true));
+	// Keep me low
+	echo memory_get_peak_usage(true);
 ?>
