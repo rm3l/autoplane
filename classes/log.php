e138fcf1369f11ac35bf59d6f52b2e77ee9d9271
GingerPaul55 <paulhappyhutchinson@gmail.com>
2012-10-27 10:33:43 +0000

    Added better support for tree listing or message and more information for HTML output

--- classes/log.php
+++ classes/log.php
@@ -3,11 +3,12 @@
 	class Log {
 		private $messages = false;	// Array messages ready for recall
 		private $level = false;		// Level of which to display messages
-		private $iii = false;
+		private $iii = false;		// Message counter
 		private $process = "Unknown";	// Process Name
 		private $rotate = 400;		// How many messages to hold before saving to file
 		private $save = true;		// Save old logs to file?
 		private $logDir = false;	// Where to save our logs
+		private $tab = 0;		// Indent tabs. Tree view
 
 		// Para 1: The name of the process or class we are monitoring
 		// Para 2: Importance of message to show. Lower you go, more debugging messages you get
@@ -67,17 +68,27 @@
 			return true;
 		}
 
+		public function tab ($count = 0) {
+			if (!is_int($count)) {
+				return false;
+			}
+			$this->tab+= $count;
+			return $this->tab;
+		}
+
 		// Para 1: The message
 		// Para 2: Level of importance, fatal errors should be high
 		public function log ($msg = false, $level = 0) {
 			if (!is_string($msg) || !is_int($level)) {
 				return false;
 			}
+
 			$GLOBALS["totalMessageCounter"]++;
 			$this->messages[$this->iii] = array(
 					"message" => &$msg,
 					"level" => &$level,
-					"time" => microtime(true)
+					"time" => microtime(true),
+					"tab" => $this->tab
 				);
 			if ($level < $this->level) {
 				$this->displayMessage($this->iii);
@@ -93,10 +104,10 @@
 			}
 			$msg = &$this->messages[$id];
 			if ($GLOBALS["cli"]) { // Command line mode
-				echo "[", $msg["time"], "]    \t", $msg["message"], PHP_EOL;
+				echo "[", $msg["time"], "]    ", str_repeat("\t", $msg["tab"] + 1) , $msg["message"], PHP_EOL;
 			} else { // HTML mode
 				echo "<span class='time'>", $msg["time"], "</span>",
-				     "<span class='message lvl", $msg["level"], "'>", $msg["message"], "</span><br />";
+				     "<span class='message lvl-", $msg["level"], " indent-",$msg["tab"] + 1," process-", $this->process,"'>", $msg["message"], "</span><br />";
 			}
 			return true;
 		}
